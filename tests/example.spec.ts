a55842957643962c19fdeaec774346bdad0bcf3a
Martin2i <89839928+martinsutherland@users.noreply.github.com>
2023-09-08 13:32:07 +0100

    tests added

---
+++ tests/example.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from '@playwright/test';
+
+test('has h1', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const header = page.locator('h1')
+
+  // Expect a title "to contain" a substring.
+  await expect(header).toHaveText("Hello, World 2023!")
+});
+
+test('has h2', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const header = page.locator('h2')
+
+  // Expect a title "to contain" a substring.
+  await expect(header).toHaveText("Hello, World 2023!")
+});
+
+test('has h3', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const header = page.locator('h3')
+
+  // Expect a title "to contain" a substring.
+  await expect(header).toHaveText("Hello, World 2023!")
+});
+
+test('has h4', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+
+  const header = page.locator('h4')
+
+  // Expect a title "to contain" a substring.
+  await expect(header).toHaveText("Hello, World 2023!")
+});
+
+
