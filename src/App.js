692da9c347b9e3c510a33a07953015b21642f753
Martin2i <89839928+martinsutherland@users.noreply.github.com>
2023-09-15 08:03:18 +0100

    removing azure file

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ const App = () => {
       
       <div className='data'>
         <div> <h1>Hello, World 2023!</h1></div>
-        <div> <h2>Hello, World 2023!</h2></div>
-        <div> <h3>Hello, World 2023!</h3></div>
-        <div> <h4>Hello, World 2023!</h4></div>
      </div>
       
    
